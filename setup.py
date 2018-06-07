4ca47ad954ada1ab715b1b0c3d948b9e12618d9e
Étienne Boisseau-Sierra <etienne.boisseau.sierra@gmail.com>
2018-06-06 20:26:23 -0400

    Wireframe setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python3
+
+
+
+def main():
+    """ Bundles the various methods """
+
+    # define file to parse
+
+    # open the file
+
+    # parse it into memory
+
+    # create a global DataFrame
+
+    # export DataFrame
+
+if __name__ == '__main__':
+    main()
